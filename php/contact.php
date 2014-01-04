2859bd71a80464d192d7c9bed38d48b3b679010c
kaijarayne <kaijarayne@gmail.com>
2014-01-04 10:49:54 -0800

    this is it

---
+++ php/contact.php
@@ -0,0 +1,42 @@
+<?php include("header.php"); ?>
+      
+<!-- CONTENT GOES HERE NOW ///////////////////////////////////////////////////// -->
+    <div class="container">
+      
+      <div class="row">
+        <div class="box">
+          <div class="col-lg-12 text-center">
+          <img class="img-responsive center-block" src="img/20110810-061222.jpg">
+              
+              <h1 id="below_spa"><small>The Spa At</small><br><span class="brand-name">Healing Touch</span><hr class="tagline-divider"><strong>with Shelly McMassagehands</strong></small></h1>
+          
+            </div>
+        </div>
+      </div>
+      
+    
+
+      <div class="row">
+        <div class="box">
+          <div class="col-lg-12 text-left" >
+            <p>HEALING TOUCH MASSAGE<br> 5483 NW EVERETT AVE.<br> PORTLAND, OR 97210<br> <a href="tel:15035837120">503&bull;519&bull;2013</a><br><a href="mailto:mcmassagehands@gmail.com">Shelly McMassagehands</a></p>
+
+            <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=5483+NW+Everett+St,+Portland,+OR&amp;aq=0&amp;oq=5483+NW+Everett+st,+Portland,+OR&amp;sll=45.525091,-122.607567&amp;sspn=0.011079,0.024784&amp;ie=UTF8&amp;hq=&amp;hnear=5483+NE+Everett+St,+Portland,+Oregon+97213&amp;t=m&amp;z=14&amp;ll=45.525091,-122.607567&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=5483+NW+Everett+St,+Portland,+OR&amp;aq=0&amp;oq=5483+NW+Everett+st,+Portland,+OR&amp;sll=45.525091,-122.607567&amp;sspn=0.011079,0.024784&amp;ie=UTF8&amp;hq=&amp;hnear=5483+NE+Everett+St,+Portland,+Oregon+97213&amp;t=m&amp;z=14&amp;ll=45.525091,-122.607567" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+          </div>
+          
+          <div class="col-lg-12">
+            <h4>TO SCHEDULE AN APPOINTMENT</h4>
+              <ul>
+                <li>Call the reception desk at Healing Touch <a href="tel:15035837120">503&bull;519&bull;2013</a></li>
+                <li>To contact me personally, and to check insurance, call <a href="tel:15035837120">503&bull;519&bull;2012</a></li>
+                <li>  If you have any additional questions about available appointment times, general questions, or insurance info, please don't hesitate to send me an <a href="mailto:mcmassagehands@gmail.com">email.</a> </li>
+              </ul>
+          </div>
+          
+        </div>
+      </div> 
+    </div>
+    
+<!-- CONTENT ENDS HERE NOW ///////////////////////////////////////////////////// -->
+    
+<?php include("footer.php"); ?>
---
+++ php/footer.php
@@ -0,0 +1,26 @@
+<footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <p>
+              <span class='st_facebook_hcount' displayText='Facebook'></span>
+              <span class='st_twitter_hcount' displayText='Tweet'></span>
+            </p>
+            <p>Copyright &copy; Healing Touch 2013</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script>
+	  // Activates the Carousel
+	  $('.carousel').carousel({
+		interval: 5000
+	  })
+	</script>
+
+  </body>
+</html>
