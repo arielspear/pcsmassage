dc0ed87e28f6ae2ada65db17edf071161e313382
trippel <cpeddecord@gmail.com>
2014-01-03 20:22:49 -0800

    Updated the php bits for fun

--- Healing Touch/php/feedback.php
+++ Healing Touch/php/feedback.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Healing Touch</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <!-- Add custom CSS here -->
-    <link href="css/healingtouch.css" rel="stylesheet">
-    <!-- Integrating Google Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Goudy+Bookletter+1911" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-    <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-    <script type="text/javascript">stLight.options({publisher: "3725fb85-fa42-44e9-a23a-d0d315f16cfd", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
-
-  </head>
-
-  <body>
-  
-    <div class="brand" id="maintitle"><a href="index.html">Healing Touch</a></div>
-    <div class="address-bar"> Healing Touch Massage| 5483 NW Everett Ave. | Portland, OR 97210 | 503.519.2013</div>
-
-    <nav class="navbar navbar-default" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.html">Business Casual</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="offerings.html">Offerings</a></li>
-            <li><a href="blog.html">Links/Feedback</a></li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container -->
-    </nav>
+<?php include("header.htm"); ?>
       
 <!-- CONTENT ////////////////////////////////////////////////////////////////////////// -->
 
@@ -139,29 +89,4 @@
       
       <!-- CONTENT ////////////////////////////////////////////////////////////////////////// -->
     
-   <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <p>
-              <span class='st_facebook_hcount' displayText='Facebook'></span>
-              <span class='st_twitter_hcount' displayText='Tweet'></span>
-            </p>
-            <p>Copyright &copy; Healing Touch 2013</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <!-- JavaScript -->
-    <script src="js/jquery-1.10.2.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script>
-	  // Activates the Carousel
-	  $('.carousel').carousel({
-		interval: 5000
-	  })
-	</script>
-
-  </body>
-</html>
+<?php include("footer.htm"); ?>
---
+++ Healing Touch/php/offerings.php
@@ -0,0 +1,70 @@
+<?php include("header.htm"); ?>
+
+        
+  <!-- CONTENT GOES HERE NOW ///////////////////////////////////////////////////// -->
+
+      <div class="container">
+
+        <div class="row">
+          
+        </div>
+        
+        <div class="row">
+          <div class="box">
+  <div class="tbody">          
+    <table class="table" width="647" style="opacity:1;">
+      <thead>
+          <tr>
+            <th>#</th>
+            <th>Offering</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+            <td>1</td>
+            <td>Hot Stone Massage</td>
+            <td>A traditional treatment releaving Chi buildup and letting stress melt away.</td>
+            <td>$45/hour</td>
+          </tr>
+          <tr>
+            <td height="29">2</td>
+            <td>Dancing Unicorn Massage</td>
+            <td>Feel the sweet magic of hoof beatings on your delicate skin while True Unicorns&trade; pound out the lumps of your flesh.</td>
+            <td>$35/hour</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Couples Massage</td>
+            <td>Be amazed as Shelly uses her powerful Demon Magic&trade; to massage two people at once.</td>
+            <td>$30/hour/person</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Deep Sea Treatment</td>
+            <td>A deep sea salt scrub followed by a massage using large kelp frawns and barnacles.</td>
+            <td>$45</td>
+          </tr>
+            <td>5</td>
+            <td>Coffee Treatment</td>
+            <td>Energizing and envigorating, a coffee skin treatment will leave your skin tight and you feeling young again.</td>
+            <td>$30</td>
+          <tr>
+
+          </tr>
+      </tbody>
+    </table>
+   </div>
+          </div>
+        </div>
+
+        <div class="row">
+          
+        </div>
+
+      </div><!-- /.container -->
+      
+  <!-- CONTENT ENDS HERE NOW ///////////////////////////////////////////////////// -->
+      
+<?php include("footer.htm"); ?>
